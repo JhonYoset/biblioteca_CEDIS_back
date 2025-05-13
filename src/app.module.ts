12c8a3191a4671a52f76bc67f790a8ad7a9401de
JhonYoset <jlunaq@unsa.edu.pe>
2025-05-13 11:56:37 -0500

    Infraestructure de la base de datos con los entities, el mapper y los repositories y tambien el config del JWT y de la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import jwtConfig from './infrastructure/config/jwt.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [jwtConfig],
+    }),
+    DatabaseModule,
+    AuthModule,
+    UsuarioModule,
+  ],
 })
 export class AppModule {}
